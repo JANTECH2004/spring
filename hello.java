9f488f6b942a85943986b0c38fa9d39d1661d1f7
JANTECH2004 <janamuniraj06@gmail.com>
2025-12-29 22:38:37 +0530

    modified hello

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ import java.util.*;
 import java.sql.*;
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");}
+        System.out.println("Hello, World!");
     }
 }
